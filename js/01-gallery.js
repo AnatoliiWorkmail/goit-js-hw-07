b270026778f1ca9296e0cb88fc80f7b5c0d4d773
AnatoliiWorkmail <jp.komod.su@gmail.com>
2022-02-14 03:48:02 +0200

    add-hw07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const selectGallery = document.querySelector('.gallery');
+
+// // 
+function createGalleryMarkup(ga) {
+    const markup = ga.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`
+    }).join('');
+
+  
+ return markup   
+}
+
+selectGallery.insertAdjacentHTML("beforeend",createGalleryMarkup(galleryItems));
+
+
+
+
+
+
+
+
